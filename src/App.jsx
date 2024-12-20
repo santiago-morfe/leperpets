572044adf3b35faa45858f1f9d4a3d265735877a
santiago <tiago01cracy@gmail.com>
2024-12-19 21:00:48 -0500

    add react-router-dom for routing and update App component with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   )
 }
 
